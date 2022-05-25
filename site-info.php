d33347333bf44116154eff7dcd720219c0b82bb1
Markus <markuslund@live.dk>
2022-05-25 10:45:07 +0200

    site info delete

---
+++ site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package Inspiro
+ * @subpackage Inspiro_Lite
+ * @since Inspiro 1.0.0
+ * @version 1.0.0
+ */
+
+?>
+<div class="site-info">
+
+</div><!-- .site-info -->
